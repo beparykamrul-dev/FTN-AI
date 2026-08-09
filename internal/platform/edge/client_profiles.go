320483b20c725ba29c78ac585261337da0972864
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-08-10 02:25:44 +0600

    feat: add FTN PC TV Android network profiles

---
+++ internal/platform/edge/client_profiles.go
@@ -0,0 +1,11 @@
+package edge
+
+type ClientPlatform string
+
+const (
+	PlatformPC ClientPlatform = "pc"
+	PlatformTV ClientPlatform = "tv"
+	PlatformAndroid ClientPlatform = "android"
+)
+
+type ClientNetworkProfile struct { DeviceID string `json:"device_id"`; Platform ClientPlatform `json:"platform"`; UserID string `json:"user_id"`; FTNNode string `json:"ftn_node"`; IP string `json:"ip,omitempty"`; MAC string `json:"mac,omitempty"`; DNS string `json:"dns,omitempty"`; Online bool `json:"online"` }
