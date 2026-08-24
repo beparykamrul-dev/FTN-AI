c226491f138fe814665e3c555a50243298aec563
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-08-25 05:10:13 +0600

    feat: add lightweight control-plane metrics endpoint

---
+++ services/control-plane/metrics.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+ "fmt"
+ "net/http"
+ "sync/atomic"
+)
+
+var requestCount uint64
+
+func metricsHandler(w http.ResponseWriter, _ *http.Request) {
+ w.Header().Set("Content-Type", "text/plain; version=0.0.4")
+ fmt.Fprintf(w, "ftn_control_requests_total %d\n", atomic.LoadUint64(&requestCount))
+}
