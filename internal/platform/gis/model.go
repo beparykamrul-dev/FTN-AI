8eb92edef4e54756d20acba4341e08cf49656239
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-04 23:26:01 -0600

    normalize GIS model formatting

--- internal/platform/gis/model.go
+++ internal/platform/gis/model.go
@@ -8,24 +8,24 @@ type Coordinate struct {
 }
 
 type IPAsset struct {
-	ID         string     `json:"id"`
-	IP         string     `json:"ip"`
-	CIDR       string     `json:"cidr,omitempty"`
-	MAC        string     `json:"mac,omitempty"`
-	Hostname   string     `json:"hostname,omitempty"`
-	ServerID   string     `json:"server_id,omitempty"`
-	Location   *Coordinate `json:"location,omitempty"`
-	Provider   string     `json:"provider,omitempty"`
-	ASN        uint32     `json:"asn,omitempty"`
-	UpdatedAt  time.Time  `json:"updated_at"`
+	ID        string      `json:"id"`
+	IP        string      `json:"ip"`
+	CIDR      string      `json:"cidr,omitempty"`
+	MAC       string      `json:"mac,omitempty"`
+	Hostname  string      `json:"hostname,omitempty"`
+	ServerID  string      `json:"server_id,omitempty"`
+	Location  *Coordinate `json:"location,omitempty"`
+	Provider  string      `json:"provider,omitempty"`
+	ASN       uint32      `json:"asn,omitempty"`
+	UpdatedAt time.Time   `json:"updated_at"`
 }
 
 type MapNode struct {
-	ID       string      `json:"id"`
-	Name     string      `json:"name"`
-	Kind     string      `json:"kind"`
-	Location Coordinate  `json:"location"`
-	IPAssets []string    `json:"ip_assets,omitempty"`
+	ID       string     `json:"id"`
+	Name     string     `json:"name"`
+	Kind     string     `json:"kind"`
+	Location Coordinate `json:"location"`
+	IPAssets []string   `json:"ip_assets,omitempty"`
 }
 
 type MapEdge struct {
