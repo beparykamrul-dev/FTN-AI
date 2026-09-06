b5980dfc92626f47555f2ee1e3b89e4459261381
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-06 14:18:49 +0600

    fix: use standard test clock

--- services/ftn-controlplane/durable_job_transition_test.go
+++ services/ftn-controlplane/durable_job_transition_test.go
@@ -1,8 +1,11 @@
 package controlplane
 
-import "testing"
+import (
+ "testing"
+ "time"
+)
 
 func TestTransitionJobRejectsTerminalMutation(t *testing.T) {
  job:=DurableJob{ID:"j1",State:JobSucceeded}
- if _,err:=TransitionJob(job,JobRunning,nowUTC()); err!=ErrInvalidJobState {t.Fatalf("got %v",err)}
+ if _,err:=TransitionJob(job,JobRunning,time.Now().UTC()); err!=ErrInvalidJobState {t.Fatalf("got %v",err)}
 }
